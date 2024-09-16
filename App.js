1bba8eb6bf344956e26d4f48b437d57c95be5098
Rex Belli Isidore Lumantao <lumantao.rex18@gmail.com>
2024-09-16 15:17:57 +0800

    Updated

--- App.js
+++ App.js
@@ -3,29 +3,14 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
-  const [isPressed, setIsPressed] = useState(false);
-  const [message, setMessage] = useState('');
-
-  const handlePress = () => {
-    setIsPressed(true); // Change state to reflect button press
-    setMessage('Yeeeeyyy mnaaa');
-  };
   return (
     <View style={styles.container}>
-      <View>
-        <Text>To-do App</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.listcont}>
-      <TouchableOpacity
-          style={[styles.button, isPressed && styles.buttonPressed]} // Apply conditional style
-          onPress={handlePress}
-        >
-          <Text style={styles.buttonText}>Done</Text>
-        </TouchableOpacity>
-        {message ? <Text style={styles.afterButtonText}>{message}</Text> : null}
-        
-      </View>
+      <View style={styles.taskWrap}>
+        <Text style={styles.title}>Tasks</Text>
+        <View style={styles.items}>
+
+        </View>
+      </ View>      
     </View>
   );
 }
@@ -35,28 +20,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#b5dcc2',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  listcont: {
-    height: 100,
-    width: 300,
-    backgroundColor: '#FF0000',
-    justifyContent: 'center',
+  taskWrap: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
   },
-  button: {
-    backgroundColor: '#e6f3eb',
-    padding: 10,
-    borderRadius: 5,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
-  buttonPressed: {
-    backgroundColor: '#76CD26', // Color when button is pressed
+  items: {
+    
   },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-  },
-  afterbuttext: {
-    color: '#098932',
-  }
+  
+  
 });
